2dc488f29795f6625aea65897507b2d41297ef5b
yatszhash <sushi.yatsuhashi@gmail.com>
2022-07-07 19:08:49 +0900

    add: support editable and placeholder

--- sort_requirements/__init__.py
+++ sort_requirements/__init__.py
@@ -3,11 +3,14 @@ import re
 VERSION = (1, 3, 0)
 DEPS_RE = r"((?:#[^\n]+?\n)*)([^\n]+?)([=!~>]=)([^\\\n]+)((?:\\\n[^\\\n]+)*)"
 WITHOUT_VERSION_RE = r"(?m)^((?:#[^\n]+?\n)*)([a-z][\w\-]*)(?=(?:\n|\Z))"
+EDITABLE_DEPENDENCIES_RE = r"(?<=\n)(\-e .+?)(?=\n)"
 
 __version__ = ".".join(str(v) for v in VERSION)
 
 
 def sort_requirements(requirements):
+    # プレースホルダーとして解釈されないようにするため
+    requirements = requirements.replace("{", "{{").replace("}", "}}")
     matches = re.findall(DEPS_RE, requirements)
     data = re.sub(DEPS_RE, "{}", requirements)
 
@@ -17,7 +20,10 @@ def sort_requirements(requirements):
     matches.extend(without_version_matches)
     matches = sorted(matches, key=lambda d: d[1].lower())
 
-    print(matches)
+    editable_matches = re.findall(EDITABLE_DEPENDENCIES_RE, data)
+    data = re.sub(EDITABLE_DEPENDENCIES_RE, "{}", data)
+    editable_matches = sorted(editable_matches, key=lambda d: d[1].lower())
+
     deps = []
     for m in matches:
         if len(m) >= 3:
@@ -25,5 +31,11 @@ def sort_requirements(requirements):
         else:
             deps.append("{}{}".format(*m))
 
+    for m in editable_matches:
+        print(m)
+        deps.append("{}".format(m))
+
+    # プレースホルダーとして解釈されないようにするため
+    #deps = [dep.replace("{", "{{").replace("}", "}}") for dep in deps]
     data = data.format(*deps)
     return data
